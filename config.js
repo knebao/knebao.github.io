7dd3e96d1c0c6796d18de6f7f858326ed7366351
knebao <116298744+knebao@users.noreply.github.com>
2022-12-20 23:54:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+  introTitle: "Hello bé iu!",
+    introDesc: "Happy anni 1y9m của hai đứa mìnhh ❤",
+  btnIntro: "Bắt đầu nà",
+  title: "Cảm ơn bé đã đồng hành cùng anh trong suốt thời gian qua",
+  desc: "Bé có yêu anh hongg",
+  btnYes: "Yêu nhiều vãii",
+  btnNo: "Hong nha bé oii",
+  question: "Hãy viết ra tâm tư tình cảm bé dành cho anh",
+  btnReply: "Chuẩn bị chưa!!",
+    mess: "Chuẩn bị cho hoàn hảo rồi hãy gửi nhá 😡",
+  messDesc: "Xong rồi thì click vào bên dưới để gửi thoy",
+  btnAccept: "Đừng làm anh cảm động quá nhaaa",
+    messLink: "https://m.me/knebao", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
